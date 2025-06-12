935b0e9bb05b18866422dffe2820b81ce9aa5e48
Alejandro Mercado <guadalupealejandro088@gmail.com>
2025-06-12 12:48:52 -0600

    Finished fizzbuzz excercice

---
+++ logica/fizzFunction.js
@@ -0,0 +1,30 @@
+ 
+
+
+
+
+//Firma de la funcion
+const firzzBuzz = (counterNumber) => {
+    let counter = 1;
+    do{
+  if(counter % 3 === 0 && counter % 5 === 0){
+    console.log(`El numero ${counter } es FizzBuzz`);
+
+  } 
+else if(counter % 3 === 0){
+  console.log(`El numero ${counter } es Fizz`);
+}
+else if(counter % 5 === 0){
+  console.log(`El numero ${counter } es Buzz`);
+}else{
+  console.log(counter);
+}
+
+counter++;
+}while(counter <= counterNumber)
+};
+
+console.log("50 veces");
+firzzBuzz(50);
+console.log("200 veces");
+firzzBuzz(200);
